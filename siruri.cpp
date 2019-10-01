9dde62d4cc4abe326a0b46ec7d213d9ef838fba0
B3-118 <meutzitzu@gmail.com>
2019-10-01 10:52:53 +0300

    01/10.2019

--- siruri.cpp
+++ siruri.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
-
+#include <fstream>
 using namespace std;
+
+void r();
 
 int main()
 {
+char C[64];
+cout<<"cerinta: ";
+cin>>C;
+
+switch(C)
+  {
+    case "palindrom" :{
+      r();
+
+    }
 
 
+  }
 
 
-  cout<<"MUEPSD";
   return 0;
 }
