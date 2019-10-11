00dc815f48a6263d53b069a87ba638900f696e33
B3-118 <meutzitzu@gmail.com>
2019-10-11 09:54:59 +0300

    2019/10/11

--- siruri.cpp
+++ siruri.cpp
@@ -1,42 +1,51 @@
 #include <iostream>
 #include <fstream>
+#include <string.h>
 using namespace std;
 
 
 
 /// MESAJE ///
-void msg(string M){
+void msg(char M[]){}
+
+/// SUBPROGRAME ///
+void help(){
+  string arg0, arg1, arg2, arg3;
+  printf(
+          "help       : listeaza comenzile posibile\n"
+          "palindrom  : determina daca un cuvant este palindrom\n"
+          "replace (\"sir\",\"sir\"): insereaza in date.in sirul dat la fiecare aparitie a sir \n",
+          arg0, arg1, arg2, arg3
+        );
+}
+
+  void palindrom(){
+    printf("NU MERE INCA\n");
+  }
 
 /// DISPECERAT ///
-void getc(){
-string C;
+int getc(){
+char C[20];
 cout<<">";
 cin>>C;
 
-if(strcmp(C, "help"))
+     if(strstr(C,            "Q!"))
+  return 0;
+else if(strstr(C,         "help"))
   help();
-if(strcmp(C, "palindrom"))
+else if(strstr(C,    "palindrom"))
   palindrom();
 else
   msg("type \"help\" for a list of commands");
+return 1;
 }
 
-void help(){
-  cout<<"help       : listeaza comenzile posibile\n
-         palindrom  : determina daca un cuvant este palindrom\n
-         replace (\"sir\",\"sir\"): insereaza in date.in sirul dat la fiecare aparitie a sir ";
-}
-//
-
-
-  cout<<'\n'<<M<<'\n';
-
-}
 
 
 int main()
 {
-
+int q=0;
+while(getc());
 
 
   return 0;
