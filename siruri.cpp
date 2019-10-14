879e53301f5408ae4f865bd022bc8df114663de3
B3-118 <meutzitzu@gmail.com>
2019-10-14 12:00:48 +0300

    2019/10/14

--- siruri.cpp
+++ siruri.cpp
@@ -6,15 +6,18 @@ using namespace std;
 
 
 /// MESAJE ///
-void msg(char M[]){}
+void msg(char M[]){
+   printf("%s\n",M );
+}
 
 /// SUBPROGRAME ///
 void help(){
   string arg0, arg1, arg2, arg3;
   printf(
-          "help       : listeaza comenzile posibile\n"
-          "palindrom  : determina daca un cuvant este palindrom\n"
-          "replace (\"sir\",\"sir\"): insereaza in date.in sirul dat la fiecare aparitie a sir \n",
+         "help          : listeaza comenzile posibile\n"
+          "palindrom    : determina daca un cuvant este palindrom\n"
+          "replace (\"sir\",\"sir\"): insereaza in date.in sirul dat la fiecare aparitie a sir \n"
+          "sufix_comun (\"sir\",\"sir\"): afiseaza cel mai mare sufix comun a celor 2 siruri",
           arg0, arg1, arg2, arg3
         );
 }
@@ -23,20 +26,42 @@ void help(){
     printf("NU MERE INCA\n");
   }
 
+  void sufix_comun(){
+      char a, b;
+      scanf("%s\n", a);
+      scanf("%s\n", b);
+      int ka=strlen(a), kb=strlen(b);
+      printf("ka%c, kb%c ... \n",ka, kb);
+      while(a[ka]==b[kb]){
+        --ka;
+        --kb;
+      }
+      while(ka>=0){
+         printf("%c",a[ka++]);
+      }
+
+
+
+  }
+
 /// DISPECERAT ///
 int getc(){
 char C[20];
 cout<<">";
 cin>>C;
 
-     if(strstr(C,            "Q!"))
+   if(strstr(C,            "Q!")){
   return 0;
-else if(strstr(C,         "help"))
-  help();
-else if(strstr(C,    "palindrom"))
-  palindrom();
-else
-  msg("type \"help\" for a list of commands");
+   }else if(strstr(C,         "help")){
+      help();
+   }else if(strstr(C,    "palindrom")){
+      palindrom();
+   }else if(strstr(C,    "sufix_comun")){
+      sufix_comun();
+   }else{
+      msg("type \"help\" for a list of commands");
+   }
+
 return 1;
 }
 
