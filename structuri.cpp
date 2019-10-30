8a9e7e7bd8316ea4a5a1140e1b69be705081b18a
B3-118 <meutzitzu@gmail.com>
2019-10-30 09:52:36 +0200

    2019/10/31

---
+++ structuri.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+
+using namespace std;
+
+void cplxcalc()
+{
+        struct cplx {
+                int r;
+                int i;
+        } z1;
+        cplx z2, zr;
+        char op;
+        
+        if()
+}
+
+
+int main ()
+{
+char C[256];
+while(1){
+        cout<<">";
+        cin>>C;
+
+                if (strstr(C,                    "Q!")){
+                        return 0;
+                } else if (strstr(C,            "TEST")){
+                        TEST();
+                } else if (strstr(C,            "cplx")){
+                        cplxcalc();
+                } else
+                        printf("type \"help\" for a list of commands");
+                }
+        }
+
+}
