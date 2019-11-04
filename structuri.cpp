f550e83d8999a21403a625f8d228a0288c5d071a
B3-118 <meutzitzu@gmail.com>
2019-11-04 12:04:56 +0200

    2019/11/04

--- structuri.cpp
+++ structuri.cpp
@@ -4,35 +4,48 @@
 
 using namespace std;
 
+void TEST()
+{
+
+}
+
 void cplxcalc()
 {
         struct cplx {
-                int r;
-                int i;
+                double r;
+                double i;
         } z1;
         cplx z2, zr;
         char op;
-        
-        if()
+        printf("Z1= ");
+        scanf("%lf %lf", z1.r, z1.i );
+        printf("Z2= ");
+        printf("%lf %lf", z1.r, z1.i );
+        //scanf("%c", op );
+        //scanf("%d", z1.r);
+        //if()
+        printf();
 }
 
 
 int main ()
 {
 char C[256];
-while(1){
-        cout<<">";
-        cin>>C;
+char q='r';
+while(q){
+        printf("\n>");
+        scanf("%s", C);
 
-                if (strstr(C,                    "Q!")){
-                        return 0;
-                } else if (strstr(C,            "TEST")){
-                        TEST();
-                } else if (strstr(C,            "cplx")){
-                        cplxcalc();
-                } else
-                        printf("type \"help\" for a list of commands");
-                }
+        if (strstr(C,                    "Q!")){
+                q=0;
+        } else if (strstr(C,            "TEST")){
+                TEST();
+        } else if (strstr(C,            "cplx")){
+                cplxcalc();
+        } else if (strstr(C,            "/pol/")){
+                printf("Hitler did nothing wrong");
+        } else
+                printf("type \"help\" for a list of commands");
         }
 
 }
