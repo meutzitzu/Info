a9cca00b2721dc3114f2503c3cbc8937bee7209f
B3-118 <meutzitzu@gmail.com>
2020-02-18 10:53:42 +0200

    2020/2/18

---
+++ TEZA_Ix11/Ace/ace.c
@@ -0,0 +1,11 @@
+#include <stdio>
+
+#define N_MAX
+#define M_MAX
+
+int main()
+{
+        int m[N_MAX][M_MAX];
+        
+
+}
--- backtracking.c
+++ backtracking.c
@@ -1,13 +1,31 @@
 #include <stdio.h>
 
+#define NUMBER_OF_ELEMENTS 10
 
-int x[10],sum[10],S,n;
+int     ix[NUMBER_OF_ELEMENTS];
+int     x[NUMBER_OF_ELEMENTS],
+        sum[NUMBER_OF_ELEMENTS],
+        S,n;
 void afis( int k)
 {
         for(int i=1;i<=k;i++)
                 printf("%d ", x[i] );
         printf("\n");
 }
+
+void def_ix()
+{
+        printf("n= " );
+        int n1;
+        scanf("%d", &n1);
+        for(int j=1; j<=n1; j++){
+                printf("ix[%d]= ", j );
+                scanf("%d", &ix[j]);
+                printf("\n");
+        }
+}
+
+//printf("MERE\n" );
 int corect(int k)
 {
         if(x[1]==0)
@@ -24,17 +42,25 @@ void bt( int k)
                 x[k]=i;
                 sum[k]=sum[k-1]+i;
                 if(corect(k)){
-                        if(k==n)afis(k);
+                        if(k==n)afis_apar(k);
                                 else bt(k+1);
                 }
         }
+}
+
+void afis_apar( int  k)
+{
+        for(int i=1;i<=k;i++)
+                if(ix[i])
+                        printf("%d ", ix[i] );
+        printf("\n");
 }
 int main()
 {
         int     x[10],
                 sum[10];
         scanf("%d %d", &n, &S );
-
+        def_ix();
         bt(1);
 
         return 0;
