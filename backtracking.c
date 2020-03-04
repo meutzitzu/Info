8f9c869ec5b4bb9d4f0648b8da87fb7ef1951319
B3-118 <meutzitzu@gmail.com>
2020-03-04 09:52:19 +0200

    2020/4/3

--- backtracking.c
+++ backtracking.c
@@ -2,24 +2,38 @@
 
 #define NUMBER_OF_ELEMENTS 10
 
-int      ix[NUMBER_OF_ELEMENTS];
+int       y[NUMBER_OF_ELEMENTS];
 int       x[NUMBER_OF_ELEMENTS],
         sum[NUMBER_OF_ELEMENTS],
         S,n;
 int       b[NUMBER_OF_ELEMENTS];
 
+struct cub {
+        char culoare[10];
+        int l;
+} a[NUMBER_OF_ELEMENTS];
+
+
 void afis( int k)
 {
-        for(int i=1;i<=k;i++)
-                printf("%d ", b[i] );
+        for ( int i = 1; i <= k; i++ )
+                printf("( %s, %d )", a[x[i]].culoare, a[x[i]].l );
         printf("\n");
+
+        if ( k > kmax ){
+                kmax = k;
+                for ( int i = 1; i <= k; i++ )
+                        y[i] = x[i];
+        }
+
+
 }
 
 void citire()
 {
         FILE* fin;
         fin = fopen("date.in", "r");
-        fscanf(fin, "%d", S);
+        fscanf(fin, "%d", n);
         int i=0;
         while( fscanf(fin, "%d[^\n]", b[i])==1 ){
                 i++;
@@ -42,21 +56,21 @@ void def_ix()
 //printf("MERE\n" );
 int corect(int k)
 {
-
+        if ( k > 1 )
+                if ( strcmp(a[x[k]].culoare, a[x[k-1]].culoare) == 0 )
+                        return 0;
         return 1;
+
 }
 
 void bt(int k)
 {
-        for (int i=x[k-1]; i<=n-sum[k-1]; i++){
-                x[k]=i;
-                sum[k]=sum[k=1]+i;
-                if(corect(k)){
-                        if(sum[k]==n&&k>1){
-                                afis(k);
-                                if(sum[k]<n)
-                                        bt(k+1);
-                        }
+        for ( int i = x[k-1] + 1; i <= n; i++ ){
+                x[k] = i;
+                if ( corect(k)){
+                        afisare(k);
+                        if ( k < n )
+                                bt(k+1);
                 }
         }
 }
@@ -71,9 +85,9 @@ void afis_apar( int  k)
 
 int main()
 {
-        scanf("%s", &n );
-        x[0]=1;
+        citire();
         bt(1);
+
 
 
 }
